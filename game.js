a3a95763ac6336c222d9e51a0dff841455a03d5e
meel3s <70290486+meel3s@users.noreply.github.com>
2021-06-17 16:09:57 -0700

    Update Tuffy

--- game.js
+++ game.js
@@ -145,7 +145,9 @@ const foreground = {
 const tuffy = {
     animation : [
         {sX: 12, sY : 18},
-        {sX:12 , sY : 58}
+        {sX: 12, sY : 18},
+        {sX: 12, sY : 58},
+        {sX: 12, sY : 58}
     ],
     x : 50,
     y : 150,
